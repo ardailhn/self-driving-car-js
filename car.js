e7b0accaa789542b08a2c7e10530387ce5a5281a
Arda İlhan <arda.ilhan@kayraexport.com>
2023-04-20 10:36:01 +0300

    neural network

--- car.js
+++ car.js
@@ -11,8 +11,12 @@ class Car {
         this.friction = 0.09;
         this.angle = 0;
 
-        if (controlType != "DUMMY")
+        this.useBrain = controlType == "AI";
+
+        if (controlType != "DUMMY") {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
+        }
         this.controls = new Controls(controlType);
     }
 
@@ -22,8 +26,18 @@ class Car {
             this.polygon = this.#createPolygon();
             this.damaged = this.#assessDamage(roadBorders, traffic);
         }
-        if (this.sensor)
+        if (this.sensor) {
             this.sensor.update(roadBorders, traffic);
+            const offsets = this.sensor.readings.map(s => s == null ? 0 : 1 - s.offset)
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+            if (this.useBrain) {
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
+        }
     }
 
     #assessDamage(roadBorders, traffic) {
@@ -106,7 +120,7 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx,color) {
+    draw(ctx, color) {
         if (this.damaged) {
             ctx.fillStyle = 'gray';
         } else {
